2ff04e9b52b662f0518de8fad01c0b85250b5644
Starlight Romero <starlightromero@gmail.com>
2020-10-30 15:21:17 -0700

    finished axios

--- axios.js
+++ axios.js
@@ -1,9 +1,24 @@
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const getBtn = document.getElementById('get-btn')
+const postBtn = document.getElementById('post-btn')
 
-const getData = () => {};
+const getData = () => {
+  axios.get('https://reqres.in/api/users').then(response => {
+    console.log(response)
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
-const sendData = () => {};
+const sendData = () => {
+  axios.post('https://reqres.in/api/register', {
+    email: 'eve.holt@reqres.com',
+    password: 'pistol'
+  }).then(response => {
+    console.log(response)
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+getBtn.addEventListener('click', getData)
+postBtn.addEventListener('click', sendData)
