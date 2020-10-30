8470e5baee1822b20892a4f5cb3a8fd82fa1645c
Starlight Romero <starlightromero@gmail.com>
2020-10-30 14:53:47 -0700

    formatted xhr

--- xhr.js
+++ xhr.js
@@ -1,39 +1,39 @@
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const getBtn = document.getElementById('get-btn')
+const postBtn = document.getElementById('post-btn')
 
 const sendHttpRequest = (method, url, data) => {
   const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url)
 
-    xhr.responseType = 'json';
+    xhr.responseType = 'json'
 
     if (data) {
-      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Content-Type', 'application/json')
     }
 
     xhr.onload = () => {
       if (xhr.status >= 400) {
-        reject(xhr.response);
+        reject(xhr.response)
       } else {
-        resolve(xhr.response);
+        resolve(xhr.response)
       }
-    };
+    }
 
     xhr.onerror = () => {
-      reject('Something went wrong!');
-    };
+      reject('Something went wrong!')
+    }
 
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
-};
+    xhr.send(JSON.stringify(data))
+  })
+  return promise
+}
 
 const getData = () => {
   sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
-    console.log(responseData);
-  });
-};
+    console.log(responseData)
+  })
+}
 
 const sendData = () => {
   sendHttpRequest('POST', 'https://reqres.in/api/register', {
@@ -41,12 +41,12 @@ const sendData = () => {
     // password: 'pistol'
   })
     .then(responseData => {
-      console.log(responseData);
+      console.log(responseData)
     })
     .catch(err => {
-      console.log(err);
-    });
-};
+      console.log(err)
+    })
+}
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+getBtn.addEventListener('click', getData)
+postBtn.addEventListener('click', sendData)
